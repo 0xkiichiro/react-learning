5bb8152b0272f79b52f1338565fcedd1898941fe
0xkiichiro <barisaytimur@gmail.com>
2022-06-28 23:08:39 +0300

    chapter-6

--- rc06_forms_otherEvents/src/components/1-forms/Form.jsx
+++ rc06_forms_otherEvents/src/components/1-forms/Form.jsx
@@ -1,9 +1,69 @@
-
+import { useState } from "react";
 
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Form
+  const handleSubmit = (e) => {
+    //! if we dont want the form to clear itself after pressing submit
+    e.preventDefault();
+    alert(
+      `Username: ${username}
+    Email: ${email}
+    Password:${password}`
+    );
+    e.target.reset();
+  };
+
+  return (
+    <div>
+      <form
+        className="d-flex flex-column justify-content-center"
+        onSubmit={() => handleSubmit()}
+      >
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            Username: <span>{username}</span>
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            aria-describedby="emailHelp"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address: <span>{email}</span>
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password:
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
--- rc06_forms_otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.js
+++ rc06_forms_otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.js
@@ -1,9 +1,51 @@
-
+import { useState } from "react";
 
 const KeyboardClipboard = () => {
-  return (
-    <div>KeyboardClipboard</div>
-  )
-}
+  const [inputData, setInputData] = useState("");
 
-export default KeyboardClipboard
+  const handleKeyDown = (e) => {
+    // console.log(e.keyCode);
+    e.target.value = e.target.value.toLocaleUpperCase();
+    e.keyCode === 13 && alert("pressing enter is cheat");
+  };
+
+  const handleAreaPaste = (e) => {
+    e.target.value = e.clipboardData.getData("text").toLocaleLowerCase();
+    e.target.style.border = "3px solid yellow";
+    e.target.style.backgroundColor = "lightcoral";
+    //! we cancel the copy/paste on browsers default
+    e.preventDefault();
+  };
+
+  const handleAreaChange = (e) => {
+    if (!e.target.value) {
+      e.target.style.border = "1px solid black";
+      e.target.style.backgroundColor = "transparent";
+    }
+  };
+
+  return (
+    <div className="container text-center">
+      <h2>Clipboard Events</h2>
+      <input
+        type="text"
+        onChange={(e) => setInputData(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <p className="text-start m-4" onCopy={() => alert("Copied!")}>
+        {inputData}
+      </p>
+
+      <textarea
+        name="area"
+        id="area"
+        cols="50"
+        rows="10"
+        onPaste={handleAreaPaste}
+        onChange={handleAreaChange}
+      ></textarea>
+    </div>
+  );
+};
+
+export default KeyboardClipboard;
