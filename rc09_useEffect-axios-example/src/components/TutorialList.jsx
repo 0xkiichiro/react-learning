a631cf0d9d4464230558b522d0f4f161e738f073
0xkiichiro <barisaytimur@gmail.com>
2022-07-02 15:07:39 +0300

    task-tracker example

---
+++ rc09_useEffect-axios-example/src/components/TutorialList.jsx
@@ -0,0 +1,55 @@
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import EditTutorial from "./EditTutorial";
+
+const TutorialList = ({ tutorials, deleteTutorial, editTutorial }) => {
+  return (
+    <div className="container mt-4">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col" className="text-center">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {tutorials?.map((item) => {
+            const { id, title, description } = item;
+            return (
+              <tr key={id}>
+                <th>{id}</th>
+                <td>{title}</td>
+                <td>{description}</td>
+                <td className="text-center">
+                  <FaEdit
+                    size={20}
+                    className="me-3 text-warning "
+                    style={{ cursor: "pointer" }}
+                    onClick={() => editTutorial(id, "title", "description")}
+                    //! to link modal to this btn
+                    //! also imported bootstrap script to index.html
+                    data-bs-toggle="modal"
+                    data-bs-target="#edit-modal"
+                  />
+                  <AiFillDelete
+                    size={22}
+                    className="text-danger"
+                    style={{ cursor: "pointer" }}
+                    onClick={() => deleteTutorial(id)}
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <EditTutorial />
+    </div>
+  );
+};
+
+export default TutorialList;
---
+++ rc09_useEffect-axios-example/src/pages/Home.jsx
@@ -0,0 +1,68 @@
+import AddTutorial from "../components/AddTutorial";
+import TutorialList from "../components/TutorialList";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const Home = () => {
+  const [tutorials, setTutorials] = useState();
+  const url = "https://axios-example-cw.herokuapp.com/api/tutorials";
+  //! get
+  const getTutorials = async () => {
+    try {
+      const { data } = await axios.get(url);
+      console.log(data);
+      setTutorials(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTutorials();
+  }, []);
+
+  //! post (create) to API
+  const addTutorial = async (tutorial) => {
+    try {
+      await axios.post(url, tutorial);
+    } catch (error) {
+      console.log(error);
+    }
+    getTutorials();
+  };
+  //! delete
+  const deleteTutorial = async (id) => {
+    try {
+      await axios.delete(`${url}/${id}`);
+    } catch (error) {
+      console.log(error);
+    }
+    getTutorials();
+  };
+  //! update (put:whole update, patch:partial update)
+  const editTutorial = async (id, title, descs) => {
+    const filtered = tutorials
+      .filter((tutorial) => tutorial.id === id)
+      .map(() => ({ title: title, description: descs }));
+    try {
+      await axios.put(`${url}/${id}`, filtered[0]);
+      console.log(filtered);
+    } catch (error) {
+      console.log(error);
+    }
+    getTutorials();
+  };
+
+  return (
+    <>
+      <AddTutorial addTutorial={addTutorial} />
+      <TutorialList
+        tutorials={tutorials}
+        deleteTutorial={deleteTutorial}
+        editTutorial={editTutorial}
+      />
+    </>
+  );
+};
+
+export default Home;
