2a9e5dcea9238fa42341896ad3dae84cc801d480
0xkiichiro <barisaytimur@gmail.com>
2022-07-05 19:00:30 +0300

    massive bugs fixed, still not working

--- rc10_task-tracker-solution/src/components/Header.jsx
+++ rc10_task-tracker-solution/src/components/Header.jsx
@@ -1,22 +1,48 @@
 import { useState } from "react";
 import AddTaskForm from "./AddTaskForm";
 
-const Header = () => {
+const Header = ({ tasks, setTasks }) => {
   const [show, setShow] = useState(false);
+  const [btnStyle, setBtnStyle] = useState({
+    name: "show add task bar",
+    bgColor: "purple",
+  });
 
-  const handleShow = () => {
+  //! React, default olarak state'leri hemen degistirmeyebilir.
+  //! Ekstra render'lari azaltmak icin state'leri toplu (batch) bir sekilde gunceller.
+  //! Bir event handler icerisindeki ardasik state'ler event bitiminde toplu bir
+  //! sekilde guncellenmis olur.State'lerin guncelenmesi gelis sirasina gorere yapilir.
+  //! Ayni event icerisinde birbirine bagli state'leri kullanirken buna dikkat etmek gerkir.
+
+  //? https://stackoverflow.com/questions/48563650/does-react-keep-the-order-for-state-updates
+
+  const handleShow = (tasks) => {
+    if (!show) {
+      setBtnStyle({
+        name: "hide add task bar",
+        bgColor: "red",
+      });
+    } else {
+      setBtnStyle({
+        name: "show add task bar",
+        bgColor: "purple",
+      });
+    }
     setShow(!show);
   };
 
   return (
     <header className="header">
-      <h1>Task Tracker</h1>
-      <button className="btn" onClick={handleShow}>
-        show add task bar
+      <h1>TASK TRACKER</h1>
+      <button
+        onClick={handleShow}
+        className="btn"
+        style={{ backgroundColor: btnStyle.bgColor }}
+      >
+        {btnStyle.name}
       </button>
-      <AddTaskForm />
+      {show && <AddTaskForm tasks={tasks} setTasks={setTasks} />}
     </header>
   );
 };
-
 export default Header;
