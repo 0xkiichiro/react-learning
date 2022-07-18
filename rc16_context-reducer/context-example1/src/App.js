2f72b1ecb6dc0f1c10f6aa9ac88eb530b09a66b2
0xkiichiro <barisaytimur@gmail.com>
2022-07-18 16:50:39 +0300

    data manupilation via useContext

--- rc16_context-reducer/context-example1/src/App.js
+++ rc16_context-reducer/context-example1/src/App.js
@@ -1,13 +1,16 @@
-import { useState } from 'react';
-import StudentList from './components/StudentList';
-import data from './data';
+import { useState } from "react";
+import StudentList from "./components/StudentList";
+import { StudentContext } from "./context/StudentContext";
+import data from "./data";
 
 function App() {
   const [students, setStudents] = useState(data);
 
   return (
     <div>
-      <StudentList students={students} />
+      <StudentContext.Provider value={{ students, setStudents }}>
+        <StudentList />
+      </StudentContext.Provider>
     </div>
   );
 }
