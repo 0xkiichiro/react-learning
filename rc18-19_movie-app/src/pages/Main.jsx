5396ab8e5a6de05e268c7ea04a1f1e8686446c97
0xkiichiro <barisaytimur@gmail.com>
2022-07-20 22:05:38 +0300

    card rendering and searchbar

--- rc18-19_movie-app/src/pages/Main.jsx
+++ rc18-19_movie-app/src/pages/Main.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const getMovies = async (API) => {
+    try {
+      setIsLoading(true);
+      const res = await axios.get(API);
+      const data = res.data.results;
+      setMovies(data);
+    } catch (err) {
+      console.log(err);
+    }
+    setIsLoading(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getMovies(SEARCH_API + search);
+  };
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
   return (
-    <div>
-      <p>Main</p>
-    </div>
+    <>
+      <form className="search" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          className="search-input"
+          placeholder="Search a movie..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div className="d-flex justify-content-center flex-wrap">
+        {isLoading ? (
+          <div className="spinner-border text-primary m-5" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        ) : (
+          movies?.map((movie) => <MovieCard key={movie.id} {...movie} />)
+        )}
+      </div>
+    </>
   );
 };
 
